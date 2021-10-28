a47820f8d80dea2594e36ecefd6a4a7b09d0006d
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-28 17:35:14 -0600

    Update core.js

--- assets/libs/js/core.js
+++ assets/libs/js/core.js
@@ -1,11 +1,12 @@
 (function() {
   const options = Object.fromEntries(new URLSearchParams(Object.assign(document.createElement('a'),{href:document.currentScript.getAttribute('src')}).search).entries())
+  const all = 'all' in options
   
-  if('include' in options) include = function(src) {
+  if('include' in options || all) include = function(src) {
     return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload: resolve})));
   }
   
-  if('throws' in options) throws = function(e) {
+  if('throws' in options || all) throws = function(e) {
     throw e;
   }
 })();
