9d7a5bb807c88132036520814f9e3c4901892270
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-28 17:32:42 -0600

    Update core.js

--- assets/libs/js/core.js
+++ assets/libs/js/core.js
@@ -1,24 +1,11 @@
 (function() {
   const options = Object.fromEntries(new URLSearchParams(Object.assign(document.createElement('a'),{href:document.currentScript.getAttribute('src')}).search).entries())
   
-  function exportConst(name, value) {
-    Object.defineProperty(globalThis, name, {
-      get() {
-        return value
-      },
-      set() {
-        throw new TypeError('Assignment to constant variable.')}
-    })
+  if('include' in options) include = function(src) {
+    return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload: resolve})));
   }
   
-  const constants = {
-    throws(e) {
-      throw e;
-    },
-    async include(src) {
-      return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload: resolve})));
-    }
+  if('throws' in options) throws = function(e) {
+    throw e;
   }
-  
-  for(const key in constants) if(key in options) exportConst(key, constants[key]);
 })();
