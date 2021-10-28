08a00f06059437db4cec3ea963f39a7191838845
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-28 17:25:11 -0600

    Update core.js

--- assets/libs/js/core.js
+++ assets/libs/js/core.js
@@ -3,12 +3,22 @@
   
   function exportConst(name, value) {
     Object.defineProperty(globalThis, name, {
-      get: () => value,
-      set: () => {
+      get() {
+        return value
+      },
+      set() {
         throw new TypeError('Assignment to constant variable.')}
     })
   }
   
-  if('throws' in options)
-    exportConst('throws', e => {throw e})
+  const constants = {
+    throws(e) {
+      throw e;
+    },
+    async include(src) {
+      return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload: resolve})));
+    }
+  }
+  
+  for(const key in constants) if(key in options) exportConst(key, constants[key]);
 })();
