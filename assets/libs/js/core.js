37839a0606b1843963636d2f7610ed119facdfdb
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-29 16:25:03 -0600

    Default to include everything

--- assets/libs/js/core.js
+++ assets/libs/js/core.js
@@ -1,6 +1,6 @@
 (function() {
   const options = Object.fromEntries(new URLSearchParams(Object.assign(document.createElement('a'),{href:document.currentScript.getAttribute('src')}).search).entries())
-  const all = 'all' in options
+  const all = 'all' in options || Object.keys(options).length === 0
   
   if('include' in options || all) include = function(src) {
     return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload: resolve})));
