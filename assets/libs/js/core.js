231df6ea3bf99da46bad9e4927970f3ecfee2c29
T Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2021-10-29 20:40:26 -0600

    Made include not return event

--- assets/libs/js/core.js
+++ assets/libs/js/core.js
@@ -2,7 +2,7 @@
     const options = Object.fromEntries(new URLSearchParams(Object.assign(document.createElement('a'),{href:document.currentScript.getAttribute('src')}).search).entries())
 
     if('include' in options) include = function(src) {
-        return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload: resolve})));
+        return new Promise(resolve => document.head.appendChild(Object.assign(document.createElement('script'), {src, onload() {resolve()}})));
     }
     
     if('throws' in options) throws = function(e) {
