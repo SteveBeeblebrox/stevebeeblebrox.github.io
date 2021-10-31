30426c4fff3de4b424da6994f25d78aa14a1d64b
TobyRue <76403689+TobyRue@users.noreply.github.com>
2021-10-31 11:43:28 -0600

    Update HtmlNode

--- assets/libs/js/domlib.js
+++ assets/libs/js/domlib.js
@@ -84,20 +84,24 @@
 
   HtmlNode = function(type, data = {}) {
     const element = document.createElement(type)
-    for(const key in data)
-      if(key in element) 
-        element[key] = data[key]
-      else 
-        element.setAttribute(key, data[key])
+    
+    if(typeof data === 'string')
+      element.textContent = data
+    else {
+      for(const key in data)
+        if(key in element) 
+          element[key] = data[key]
+        else 
+          element.setAttribute(key, data[key])
 
-    if('children' in data && data.children instanceof Array)
-      for(const child of data.children)
-        element.appendChild(child)
+      if('children' in data && data.children instanceof Array)
+        for(const child of data.children)
+          element.appendChild(child)
 
-    if('style' in data && typeof(data.style) === 'object')
-      for(const property in data.style)
-        element.style[property] = data.style[property]
-
+      if('style' in data && typeof(data.style) === 'object')
+        for(const property in data.style)
+            element.style[property] = data.style[property]
+    }
     return element
   }
 
