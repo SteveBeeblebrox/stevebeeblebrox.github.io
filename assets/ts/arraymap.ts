64f83f08cb64360826b60b5c654cdbdbaa85693d
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2022-05-05 18:10:48 -0600

    Update arraymap.ts

--- assets/ts/arraymap.ts
+++ assets/ts/arraymap.ts
@@ -2,27 +2,61 @@ type SomeArray<V> = V[] & {0:V}
 class ArrayMap<K,V> implements Map<SomeArray<K>,V | ArrayMap<K,V>> {
     constructor(private readonly map: Map<K,V | ArrayMap<K,V>> = new Map()){}
     // impl Map
+    /**
+     * Remove all children and values from this map
+     */
     clear(): void {
         return this.map.clear();
     }
-    delete(keys: SomeArray<K>): boolean {
-        return this.apply(keys,(key,map)=>map.delete(key));
+    /**
+     * Remove the value or map at `keys`
+     * 
+     * @param `keys` The array to lookup
+     * @param `force` If false, an error is thrown when unable to lookup `keys`; otherwise, it silently completes
+     */
+    delete(keys: SomeArray<K>, force = true): boolean {
+        return this.apply(keys,(key,map)=>map.delete(key), force ? 'transparent' : 'fail');
     }
     forEach(callbackfn: (value: ArrayMap<K,V>|V,key: SomeArray<K>,map: Map<SomeArray<K>,ArrayMap<K,V>|V>) => void,thisArg?: any): void {
         this.map.forEach((value, key) => callbackfn.call(thisArg, value, [key], this), thisArg);
     }
-    get(keys: SomeArray<K>): ArrayMap<K,V>|V|undefined {
-        return this.apply(keys,(key,map)=>map.get(key));
+    /**
+     * Retrieve the value at `keys`
+     * 
+     * @param `keys` The array to lookup
+     * @param `force` If false, an error is thrown when unable to lookup `keys`; otherwise, it silently completes and returns undefined
+     */
+    get(keys: SomeArray<K>, force = true): ArrayMap<K,V>|V|undefined {
+        return this.apply(keys,(key,map)=>map.get(key), force ? 'transparent' : 'fail');
     }
-    has(keys: SomeArray<K>): boolean {
-        return this.apply(keys,(key,map)=>map.has(key));
+    /**
+     * Does this map (ignoring children) have some value at `keys`
+     * 
+     * @param `keys` The array to lookup
+     * @param `force` If false, an error is thrown when unable to lookup `keys`; otherwise, it silently completes and returns false
+     */
+    has(keys: SomeArray<K>, force = true): boolean {
+        return this.apply(keys,(key,map)=>map.has(key), force ? 'transparent' : 'fail');
     }
-    set(keys: SomeArray<K>,value: ArrayMap<K,V>|V, overwrite = true): this {
-        this.apply(keys, (key, map)=>map.set(key,value), overwrite);
+    /**
+     * Sets the value at `keys` to `value`
+     * 
+     * @param `keys` The array to lookup
+     * @parm `value` THe new value
+     * @param `force` If false, an error is thrown when unable to lookup `keys`; otherwise, it silently completes
+     */
+    set(keys: SomeArray<K>,value: ArrayMap<K,V>|V, force = true): this {
+        this.apply(keys, (key, map)=>map.set(key,value), force ? 'overwrite' : 'fail');
         return this;
     }
-    init(keys: SomeArray<K>, overwrite = true): this {
-        this.apply(keys, (key, map)=>map.set(key, new ArrayMap()), overwrite);
+    /**
+     * Recursively creates maps matching to `keys`
+     * 
+     * @param `keys` The array to lookup
+     * @param `force` If false, an error is thrown when unable to lookup `keys`; otherwise, it silently completes
+     */
+    init(keys: SomeArray<K>, force = true): this {
+        this.apply(keys, (key, map)=>map.set(key, new ArrayMap()), force ? 'overwrite' : 'fail');
         return this;
     }
     get size(): number {
@@ -52,26 +86,27 @@ class ArrayMap<K,V> implements Map<SomeArray<K>,V | ArrayMap<K,V>> {
         return [...array];
     }
     
-    private resolve(keys: SomeArray<K>, overwrite = false): [K, ArrayMap<K,V>] {
+    private resolve(keys: SomeArray<K>, mode: 'overwrite' | 'transparent' | 'fail' = 'fail'): [K, ArrayMap<K,V>] {
         keys=ArrayMap.copy(keys) as SomeArray<K>;
         return [keys.pop()!, keys.reduce(function(map, key, index) {
             let value = map.get([key]);
             if(!(value instanceof ArrayMap) || value === undefined) {
-                if(overwrite)
+                if(mode === 'overwrite' || (mode === 'transparent' && value === undefined))
                     map.set([key], value = new ArrayMap<K,V>())
-                else // fake it
+                else if(mode === 'transparent') // fake it
                     value = new ArrayMap();
+                else throw new Error(`Unable to resolve key, expected map but found "${value}"`);
             }
             return value;
         }, this as ArrayMap<K,V>)];
     }
 
-    private apply<R>(keys: SomeArray<K>, callback: (key:K,map:Map<K,ArrayMap<K,V> | V>)=>R, overwrite = false) {
+    private apply<R>(keys: SomeArray<K>, callback: (key:K,map:Map<K,ArrayMap<K,V> | V>)=>R, mode: 'overwrite' | 'transparent' | 'fail' = 'fail') {
         if(keys.length === 1) {
             return callback(keys[0],this.map as Map<K,V>)
         }
         else {
-            const [key, map] = this.resolve(keys, overwrite);
+            const [key, map] = this.resolve(keys, mode);
             return callback([key] as unknown as K, map as unknown as Map<K,V>);
         }
     }
