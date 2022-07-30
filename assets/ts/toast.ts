0b0d96289cc315812e28663040ab322f8504c073
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2022-07-29 19:03:36 -0600

    Toast update

--- assets/ts/toast.ts
+++ assets/ts/toast.ts
@@ -20,19 +20,32 @@ const toast = (function() {
                 transition: opacity 0.15s linear;
                 align-self: flex-end;
                 margin-bottom: 2em;
-                border-radius: 99999px;
+                border-radius: 3ch;
                 z-index: 99999;
                 color: #444;
                 background-color: rgb(245 245 245 / 85%);
                 box-shadow:0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
-                padding: 0.5em 1em;
-                pointer-events: all;
-                cursor: pointer;
+                padding: 1.5ch 2ch;
+                pointer-events: none;
+                user-select: none;
                 opacity: 0;
+            }
+            :host>span>span {
+                --max-lines: 4;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: var(--max-lines);
+                -webkit-box-orient: vertical;
+                max-width: 20ch;
             }
         `
     }));
-    const span = document.createElement('span')
+
+    const span = document.createElement('span');
+    const textSpan = document.createElement('span');
+
+    span.appendChild(textSpan);
     shadowRoot.appendChild(span);
     
     const durations = {
@@ -50,12 +63,12 @@ const toast = (function() {
                 await Promise.all(pendingToasts);
                 clearTimeout(resetTimeout)
                 pendingToasts.push(new Promise(function(resolveCompleted) {
-                    span.textContent = text;
+                    textSpan.textContent = text;
                     span.style.opacity = '1.0';
                     setTimeout(function() {
                         span.style.opacity = '0.0';
                         resetTimeout = setTimeout(function() {
-                            span.textContent = '';
+                            textSpan.textContent = '';
                             resolveReturn();
                         }, 250);
                     pendingToasts.shift();
