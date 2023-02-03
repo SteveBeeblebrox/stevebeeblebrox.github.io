6c490e66f300d6fa78a8fc84de820b810c1a7c49
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-02-03 16:54:56 -0700

    Update

---
+++ assets/ts/keybinds.ts
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2022 S. Beeblebrox
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+class Keybinds {
+   private static readonly CommandKeys = (function() {
+      enum CommandKeys {
+         Control = 'control',
+         Shift = 'shift',
+         Alt = 'alt',
+         Meta = 'meta',
+         Windows = 'windows'
+      }
+      return CommandKeys; 
+   })();
+   private static readonly DEFAULT_INSTANCE = new Keybinds();
+   private static readonly COMMAND_KEYS_ARRAY = Object.values(Keybinds.CommandKeys).filter(x=>typeof x === 'string') as string[];
+   private static readonly COMMAND_KEYS = Keybinds.COMMAND_KEYS_ARRAY.filter(x=>x!=Keybinds.CommandKeys.Windows);
+   private readonly levels = Array.apply(null, {length:5} as unknown[]).map(_=>new Map()) as Map<{char: string, ctrlKey: boolean, shiftKey: boolean, altKey: boolean, metaKey: boolean},Function>[];
+   constructor(private readonly target: EventTarget = window) {
+         target.addEventListener('keydown',this.onEvent);
+   }
+   static on(pattern: string, f: Function) {
+      return Keybinds.DEFAULT_INSTANCE.on(pattern, f)
+   }
+   on(pattern: string, f: Function) {
+      const reqs = pattern.split(/\s+[+,]?\s?/).map(o=>o.toLowerCase().replaceAll(/^ctrl$/g,Keybinds.CommandKeys.Control));
+      this.levels[Math.min(reqs.length-1,4)]!.set({
+         ctrlKey: reqs.includes(Keybinds.CommandKeys.Control),
+         shiftKey: reqs.includes(Keybinds.CommandKeys.Shift),
+         altKey: reqs.includes(Keybinds.CommandKeys.Alt),
+         metaKey: reqs.includes(Keybinds.CommandKeys.Meta) || reqs.includes(Keybinds.CommandKeys.Windows),
+         char: reqs.find(c=>!Keybinds.COMMAND_KEYS_ARRAY.includes(c)) ?? (()=>{throw new Error(`Keybind '${pattern}' must contain a none command key`)})()
+      },f);
+   }
+   detach() {
+      this.target.removeEventListener('keydown',this.onEvent)
+   }
+   private readonly onEvent = (function(this: Keybinds, event: KeyboardEvent) {
+      if((Keybinds.COMMAND_KEYS as string[]).includes(event.key.toLowerCase())) return;
+      const length = +event.ctrlKey + +event.shiftKey + +event.altKey + +event.metaKey;
+      const f  = [...this.levels[length].entries()].find(function([reqs]) {
+         return (!reqs.ctrlKey || event.ctrlKey)
+         && (!reqs.shiftKey || event.shiftKey)
+         && (!reqs.altKey || event.altKey)
+         && (!reqs.metaKey || event.metaKey)
+         && reqs.char === event.key.replace(/^ $/,'Space').toLowerCase();
+      })?.[1];
+      if(f) {
+         event.preventDefault();
+         f();
+      }
+   }).bind(this) as EventListener;
+}
