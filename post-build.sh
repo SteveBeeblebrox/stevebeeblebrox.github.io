008ea4888cc76029ea1cebc3e26e198237022c13
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-02-08 19:26:44 -0700

    Update

--- post-build.sh
+++ post-build.sh
@@ -1,8 +1,8 @@
 # vfs.ts does not work minified
 VFS_SOURCE="assets/ts/vfs.ts"
-VFS_OUT=$("$WYVERN_DIR/pt".exe $VFS_SOURCE ts~js '(?<=\.m|\.)tsx?$~~js')
+VFS_OUT=$("$WYVERN_DIR/pt$EXE_SUFFIX" $VFS_SOURCE ts~js '(?<=\.m|\.)tsx?$~~js')
 if [ "$VFS_SOURCE" -nt "$VFS_OUT" ]; then
-    "$WYVERN_DIR/mtsc.exe" --target=es2019 --jsx=JSX.createElement $VFS_SOURCE --out=- > $VFS_OUT
+    "$WYVERN_DIR/mtsc$EXE_SUFFIX" --target=es2019 --jsx=JSX.createElement $VFS_SOURCE --out=- > $VFS_OUT
 fi;
 # remove broken file
-rm "$("$WYVERN_DIR/pt".exe $VFS_SOURCE ts~js '(?<=\.m|\.)tsx?$~~js' '(?=\..?js.?$)~~.min')"
+rm "$("$WYVERN_DIR/pt$EXE_SUFFIX" $VFS_SOURCE ts~js '(?<=\.m|\.)tsx?$~~js' '(?=\..?js.?$)~~.min')"
