af5c33ec60fc41852c97fd03df81f39e6ddf17cd
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-09-11 14:21:22 -0600

    Update random.ts

--- assets/ts/random.ts
+++ assets/ts/random.ts
@@ -23,14 +23,14 @@ class Random {
     private static IMPLEMENTATIONS = {
         fast(seed) {
             let [state] = Random.splitSeed(seed, 1, 53);
-            return function random() {
+            return function() {
                 const x = Math.sin(state++) * 10000;
                 return (x - Math.floor(x));
             }
         },
         mwc(seed) {
             let [a, b] = Random.splitSeed(seed, 2, 16);
-            return function () {
+            return function() {
                 a = 36969 * (a & 65535) + (a >>> 16);
                 b = 18000 * (b & 65535) + (b >>> 16);
                 var result = (a << 16) + (b & 65535) >>> 0;
@@ -39,14 +39,14 @@ class Random {
         },
         xorshift32(seed) {
             let [a] = Random.splitSeed(seed);
-            return function () {
+            return function() {
                 a ^= a << 13; a ^= a >>> 17; a ^= a << 5;
                 return (a >>> 0) / 4294967296;
             }
         },
         xorshift128(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            return function () {
+            return function() {
                 var t = a ^ a << 11;
                 a = b, b = c, c = d;
                 d = (d ^ d >>> 19) ^ (t ^ t >>> 8);
@@ -55,7 +55,7 @@ class Random {
         },
         xorwow(seed) {
             let [a, b, c, d, e, f] = Random.splitSeed(seed, 6);
-            return function () {
+            return function() {
                 var t = a ^ a >>> 2;
                 a = b, b = c, c = d, d = e;
                 e = (e ^ e << 4) ^ (t ^ t << 1);
@@ -65,14 +65,14 @@ class Random {
         },
         xorshift32m(seed) {
             let [a] = Random.splitSeed(seed);
-            return function () {
+            return function() {
                 a ^= a << 13; a ^= a >>> 17; a ^= a << 5;
                 return (Math.imul(a, 1597334677) >>> 0) / 4294967296;
             }
         },
         xorshift32amx(seed) {
             let [a] = Random.splitSeed(seed);
-            return function () {
+            return function() {
                 var t = Math.imul(a, 1597334677);
                 t = t >>> 24 | t >>> 8 & 65280 | t << 8 & 16711680 | t << 24; // reverse byte order
                 a ^= a << 13; a ^= a >>> 17; a ^= a << 5;
@@ -81,7 +81,7 @@ class Random {
         },
         xoroshiro64ss(seed) {
             let [a, b] = Random.splitSeed(seed, 2);
-            return function () {
+            return function() {
                 var r = Math.imul(a, 0x9E3779BB); r = (r << 5 | r >>> 27) * 5;
                 b = b ^ a; a = b ^ (a << 26 | a >>> 6) ^ b << 9;
                 b = b << 13 | b >>> 19;
@@ -90,7 +90,7 @@ class Random {
         },
         xoroshiro64s(seed) {
             let [a, b] = Random.splitSeed(seed, 2);
-            return function () {
+            return function() {
                 var r = Math.imul(a, 0x9E3779BB);
                 b = b ^ a; a = b ^ (a << 26 | a >>> 6) ^ b << 9;
                 b = b << 13 | b >>> 19;
@@ -99,7 +99,7 @@ class Random {
         },
         xoroshiro64p(seed) {
             let [a, b] = Random.splitSeed(seed, 2);
-            return function () {
+            return function() {
                 var r = a + b;
                 b = b ^ a; a = b ^ (a << 26 | a >>> 6) ^ b << 9;
                 b = b << 13 | b >>> 19;
@@ -108,7 +108,7 @@ class Random {
         },
         xoroshiro128plus_32(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            const r = function () {
+            const r = function() {
                 var x = a >>> 0,
                     y = b >>> 0,
                     z = c >>> 0,
@@ -130,7 +130,7 @@ class Random {
         },
         xorshift128plus_32b(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            const r = function () {
+            const r = function() {
                 var x = a >>> 0,
                     y = b >>> 0,
                     z = c >>> 0,
@@ -152,7 +152,7 @@ class Random {
         },
         xoshiro128ss(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            return function () {
+            return function() {
                 var t = b << 9, r = a * 5; r = (r << 7 | r >>> 25) * 9;
                 c = c ^ a; d = d ^ b; b = b ^ c; a = a ^ d; c = c ^ t;
                 d = d << 11 | d >>> 21;
@@ -161,7 +161,7 @@ class Random {
         },
         xoshiro128pp(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            return function () {
+            return function() {
                 var t = b << 9, r = a + d; r = (r << 7 | r >>> 25) + a;
                 c = c ^ a; d = d ^ b; b = b ^ c; a = a ^ d; c = c ^ t;
                 d = d << 11 | d >>> 21;
@@ -170,7 +170,7 @@ class Random {
         },
         xoshiro128p(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            return function () {
+            return function() {
                 var t = b << 9, r = a + d;
                 c = c ^ a; d = d ^ b; b = b ^ c; a = a ^ d; c = c ^ t;
                 d = d << 11 | d >>> 21;
@@ -179,7 +179,7 @@ class Random {
         },
         jsf32(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            const r = function () {
+            const r = function() {
                 a |= 0; b |= 0; c |= 0; d |= 0;
                 var t = a - (b << 27 | b >>> 5) | 0;
                 a = b ^ (c << 17 | c >>> 15);
@@ -193,7 +193,7 @@ class Random {
         },
         jsf32b(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            const r = function () {
+            const r = function() {
                 a |= 0; b |= 0; c |= 0; d |= 0;
                 var t = a - (b << 23 | b >>> 9) | 0;
                 a = b ^ (c << 16 | c >>> 16) | 0;
@@ -208,7 +208,7 @@ class Random {
         },
         gjrand32(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            const r = function () {
+            const r = function() {
                 a |= 0; b |= 0; c |= 0; d |= 0;
                 a = a << 16 | a >>> 16;
                 b = b + c | 0;
@@ -228,7 +228,7 @@ class Random {
         },
         sfc32(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            const r = function () {
+            const r = function() {
                 a |= 0; b |= 0; c |= 0; d |= 0;
                 var t = (a + b | 0) + d | 0;
                 d = d + 1 | 0;
@@ -243,7 +243,7 @@ class Random {
         },
         tychei(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            return function () {
+            return function() {
                 a |= 0; b |= 0; c |= 0; d |= 0;
                 b = (b << 25 | b >>> 7) ^ c; c = c - d | 0;
                 d = (d << 24 | d >>> 8) ^ a; a = a - b | 0;
@@ -254,7 +254,7 @@ class Random {
         },
         tyche(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
-            return function () {
+            return function() {
                 a |= 0; b |= 0; c |= 0; d |= 0;
                 a = a + b | 0; d = d ^ a; d = d << 16 | d >>> 16;
                 c = c + d | 0; b = b ^ c; b = b << 12 | b >>> 20;
@@ -265,7 +265,7 @@ class Random {
         },
         mulberry32(seed) {
             let [a] = Random.splitSeed(seed);
-            return function () {
+            return function() {
                 a |= 0; a = a + 0x6D2B79F5 | 0;
                 var t = Math.imul(a ^ a >>> 15, 1 | a);
                 t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
@@ -274,7 +274,7 @@ class Random {
         },
         splitmix32(seed) {
             let [a] = Random.splitSeed(seed);
-            return function () {
+            return function() {
                 a |= 0; a = a + 0x9e3779b9 | 0;
                 var t = a ^ a >>> 15; t = Math.imul(t, 0x85ebca6b);
                 t = t ^ t >>> 13; t = Math.imul(t, 0xc2b2ae35);
@@ -284,7 +284,7 @@ class Random {
         v3b(seed) {
             let [a, b, c, d] = Random.splitSeed(seed, 4);
             var out: number, pos = 0, a0 = 0, b0 = b, c0 = c, d0 = d;
-            const r = function () {
+            const r = function() {
                 if (pos === 0) {
                     a += d; a = a << 21 | a >>> 11;
                     b = (b << 12 | b >>> 20) + c;
