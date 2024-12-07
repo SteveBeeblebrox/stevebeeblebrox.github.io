ef1aa56648f8bda7d50694cec12cb6588563e1fe
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-12-07 12:36:39 -0700

    Update

--- assets/ts/encryption.ts
+++ assets/ts/encryption.ts
@@ -1,34 +1,47 @@
 /**
  * AES-GCM Encryption
  * (c) 2017 Chris Veness - MIT Licence
+ * Adapted for TypeScript and buffers
  * https://gist.github.com/chrisveness/43bcda93af9f646d083fad678071b90a
  */
- namespace AESGCMEncryption {
+namespace AESGCMEncryption {
     export async function encrypt(plaintext: string, password: string): Promise<string> {
-        const passwordUtf8 = new TextEncoder().encode(password)
-        const passwordHash = await crypto.subtle.digest('SHA-256', passwordUtf8)
-        const iv = crypto.getRandomValues(new Uint8Array(12))
-        const algorithm = { name: 'AES-GCM', iv: iv }
-        const key = await crypto.subtle.importKey('raw', passwordHash, algorithm, false, ['encrypt'])
-        const ptUint8 = new TextEncoder().encode(plaintext)
-        const ctBuffer = await crypto.subtle.encrypt(algorithm, key, ptUint8)
-        const ctArray = Array.from(new Uint8Array(ctBuffer))
-        const ctStr = ctArray.map(byte => String.fromCharCode(byte)).join('')
-        const ctBase64 = btoa(ctStr)
-        const ivHex = Array.from(iv).map(b => ('00' + b.toString(16)).slice(-2)).join('')
-        return ivHex + ctBase64
+        const sha256 = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(password));
+        const algorithm = {name: 'AES-GCM', iv: crypto.getRandomValues(new Uint8Array(12))};
+        const key = await crypto.subtle.importKey('raw', sha256, algorithm, false, ['encrypt']);
+        const ctBuffer = await crypto.subtle.encrypt(algorithm, key, new TextEncoder().encode(plaintext));
+        const ctBase64 = btoa(Array.from(new Uint8Array(ctBuffer)).map(byte => String.fromCharCode(byte)).join(''));
+        const ivHex = Array.from(algorithm.iv).map(b => b.toString(16).padStart(2,'0')).join('');
+        return ivHex + ctBase64;
     }
 
     export async function decrypt(ciphertext: string, password: string): Promise<string> {
-        const passwordUtf8 = new TextEncoder().encode(password)
-        const passwordHash = await crypto.subtle.digest('SHA-256', passwordUtf8)
-        const iv = ciphertext.slice(0,24).match(/.{2}/g)!.map(byte => parseInt(byte, 16))
-        const algorithm = { name: 'AES-GCM', iv: new Uint8Array(iv) }
-        const key = await crypto.subtle.importKey('raw', passwordHash, algorithm, false, ['decrypt'])
-        const ctStr = atob(ciphertext.slice(24))
-        const ctUint8 = new Uint8Array(ctStr.match(/[\s\S]/g)!.map(ch => ch.charCodeAt(0)))
-        const plainBuffer = await crypto.subtle.decrypt(algorithm, key, ctUint8)
-        const plaintext = new TextDecoder().decode(plainBuffer)
-        return plaintext
+        const sha256 = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(password));
+        const algorithm = {name: 'AES-GCM', iv: new Uint8Array(ciphertext.slice(0,24).match(/.{2}/g)!.map(byte => parseInt(byte, 16)))};
+        const key = await crypto.subtle.importKey('raw', sha256, algorithm, false, ['decrypt']);
+        const ctStr = atob(ciphertext.slice(24));
+        const ctUint8 = new Uint8Array(ctStr.match(/[\s\S]/g)!.map(ch => ch.charCodeAt(0)));
+        return new TextDecoder().decode(await crypto.subtle.decrypt(algorithm, key, ctUint8));
+    }
+
+    function concatBuffers(left: ArrayBuffer, right: ArrayBuffer): ArrayBuffer {
+        const x = new Uint8Array(left.byteLength + right.byteLength);
+        x.set(new Uint8Array(left),0);
+        x.set(new Uint8Array(right),left.byteLength);
+        return x;
+    }
+
+    export async function encryptBuffer(plaintext: ArrayBuffer, password: string): Promise<ArrayBuffer> {
+        const sha256 = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(password));
+        const algorithm = {name: 'AES-GCM', iv: crypto.getRandomValues(new Uint8Array(12))};
+        const key = await crypto.subtle.importKey('raw', sha256, algorithm, false, ['encrypt']);
+        return concatBuffers(algorithm.iv, await crypto.subtle.encrypt(algorithm, key, plaintext));
+    }
+
+    export async function decryptBuffer(ciphertext: ArrayBuffer, password: string): Promise<ArrayBuffer> {
+        const sha256 = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(password));
+        const algorithm = {name: 'AES-GCM', iv: new Uint8Array(ciphertext.slice(0,12))};
+        const key = await crypto.subtle.importKey('raw', sha256, algorithm, false, ['decrypt']);
+        return await crypto.subtle.decrypt(algorithm, key, ciphertext.slice(12));
     }
 }
